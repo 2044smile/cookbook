cb9cc73ccccd5e63654a6045d595c6110a21575b
2044smile <2044smile@naver.com>
2022-09-04 21:37:52 +0900

    refactor: DEFAULT_AUTO_FIELD and Django version 2.x

--- entities/apps.py
+++ entities/apps.py
@@ -1,5 +1,7 @@
+from cookbook.settings import DEFAULT_AUTO_FIELD
 from django.apps import AppConfig
 
 
 class EntitiesConfig(AppConfig):
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
     name = 'entities'
